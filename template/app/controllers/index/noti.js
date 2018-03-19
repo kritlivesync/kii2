10596f5a35f5da94a771a8ec6cd703f870b67f44
Chakkrit Tunyong <chakkrit@livesync.io>
2018-03-19 11:09:14 +0700

    update noti

--- template/app/controllers/index/noti.js
+++ template/app/controllers/index/noti.js
@@ -3,7 +3,7 @@ module.exports = async(app) => {
         .route('/noti')
         .post((req, res) => {
           var id = Date.now();
- 		  R.publish("public", JSON.stringify({data:is}));
+ 		  R.publish("public", JSON.stringify({data:id}));
           res.send({ result: 'OK', message: 'Send noti to socket '+id });
         });
 };
